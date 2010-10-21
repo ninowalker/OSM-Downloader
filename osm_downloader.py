028dc68969f24021a882c5461ee2f47ff47b2543
Nino Walker <nino.walker@gmail.com>
2010-10-20 22:42:22 -0700

    Added a downloader.

--- osm_downloader.py
+++ osm_downloader.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python
-
 import datetime
 import math
+from progressbar import ProgressBar
+import urllib
+import os, stat, time
 
 ## {{{ http://code.activestate.com/recipes/66472/ (r1)
 def frange(start, end=None, inc=None):
@@ -33,7 +34,7 @@ class Tile(object):
     def url(self):
         return "http://api.openstreetmap.org/api/0.6/map?bbox=%f,%f,%f,%f" % self.bounds
 
-class DownloadRegion(object):
+class Region(object):
     def __init__(self, bounds, step=0.04, overlap=0.001, name=None):
         self.bounds = bounds
         if not name:
@@ -67,9 +68,35 @@ class DownloadRegion(object):
     @property
     def top(self): return self.bounds[3]
 
+class Downloader(object):
+    def __init__(self, dir, max_age=None):
+        self.dir = dir
+        self.max_age = max_age
+
+    def download(self, region, callback=None):
+        tiles = list(region.tiles())
+        pbar = ProgressBar(maxval=len(tiles))
+        pbar.start()
+        for i,tile in enumerate(tiles):
+            f = self._download(tile)
+            if callback:
+                callback(f)
+            pbar.update(i+1)
+        pbar.finish()
+        print "Downloads complete"
+
+    def _download(self, tile):
+        filename = os.path.join(self.dir, "%f_%f__%f_%f.osm.xml" % tile.bounds)
+        if os.path.exists(filename):
+            if not self.max_age:
+                return filename
+            if os.stat(filename)[stat.ST_MTIME] + self.max_age > time.time():
+                return filename
+        d = urllib.urlopen(tile.url).read()
+        with open(os.path.join(self.dir, filename),"w") as f:
+            f.write(d)
+        return filename
 
 if __name__ == '__main__':
-    ts = list(DownloadRegion((0,0,0.04,0.04), step=0.01, overlap=0.001).tiles())
-    for t in ts:
-        print t
-    print len(ts)
+    d = Downloader("/tmp", 5)
+    d.download(Region((0,0,1,1), step=0.2))
--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import unittest
-from osm_downloader import DownloadRegion
+from osm_downloader import Region
 
 class Tester(unittest.TestCase):
 
     def testGeneratesNum(self):
-        self.assertEquals(len(list(DownloadRegion((0,0,0.04,0.04), step=0.01).tiles())),
+        self.assertEquals(len(list(Region((0,0,0.04,0.04), step=0.01).tiles())),
                           16)
-        self.assertEquals(len(list(DownloadRegion((0,0,0.01,0.01), step=0.01).tiles())),
+        self.assertEquals(len(list(Region((0,0,0.01,0.01), step=0.01).tiles())),
                           1)
-        self.assertEquals(len(list(DownloadRegion((0,0,0.1,0.01), step=0.01).tiles())),
+        self.assertEquals(len(list(Region((0,0,0.1,0.01), step=0.01).tiles())),
                           10)
-        self.assertEquals(len(list(DownloadRegion((-180,-90,180,90), step=1).tiles())),
+        self.assertEquals(len(list(Region((-180,-90,180,90), step=1).tiles())),
                           360*180)
         
 
